1cc3a2b982d7b685121be4431a3f33fa24e997a7
lixyleon <lixyleon2000@gmail.com>
2023-06-10 12:25:24 -0400

    finish Header

---
+++ include/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
+</head>
+<body>
+  <header>
+    <nav class="navbar bg-primary ">
+      <div class="container-fluid">
+        <a class="navbar-brand text-white" href="#" >
+          <i class="bi bi-bell-fill"></i>To Do Page
+        </a>
+      </div>
+    </nav>
+  </header>
+  <main>
