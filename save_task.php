23191140b38d371fcbeeed917f9c5350411eb7d2
lixyleon <lixyleon2000@gmail.com>
2023-07-08 12:37:11 -0400

    finish create form new task

---
+++ save_task.php
@@ -0,0 +1,34 @@
+<?php 
+include("database/db.php");
+if(isset($_POST['save_task'])){
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $image =getimagesize($_FILES['image']['tmp_name']);
+    if($image !== false){
+        $image = $_FILES['image']['tmp_name'];
+        $img_content = addslashes(file_get_contents($image));
+    }else{
+        $img_content ='';
+    }
+
+    $query= "INSERT INTO task (title, description, image) VALUES ('$title','$description','$img_content')";
+    $result = mysqli_query($conn, $query);
+    if(!$result){
+      //  die(mysqli_erro($mysqli));
+      
+      die("query failed");
+    }
+
+    $_SESSION['message']= 'Task Saved Successfully';
+    $_SESSION['message_type']= 'success';
+    header("Location:index.php");
+
+}
+
+
+
+
+
+
+
+?>
